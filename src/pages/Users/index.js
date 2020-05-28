1e090603d26215f12335768437cd4255d9919d57
giovannimaffeo <giovanni.maffeo@gmail.com>
2020-05-28 02:08:52 -0300

    part_two-mapintro

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -3,10 +3,38 @@ import { Link } from "react-router-dom";
 
 class Users extends Component {
   render() {
+    const users = [
+      {
+        id: 1,
+        user: "João"
+      },
+      {
+        id: 2,
+        user: "Maria"
+      },
+      {
+        id: 3,
+        user: "Júlia"
+      },
+      {
+        id: 4,
+        user: "Felipe"
+      },
+      {
+        id: 5,
+        user: "João"
+      }
+    ];
+    const usersList = users.map(item => (
+      <li key={item.id.toString()}>{item.user}</li>
+    ));
     return (
       <div>
-        <h1>Users</h1>
-        <Link to="/details">Ir para detalhes</Link>
+        <div>
+          <h1>Users</h1>
+          <Link to="/details">Ir para detalhes</Link>
+        </div>
+        <ul>{usersList}</ul>
       </div>
     );
   }
